2acd1fefbf7df6311de217d8c4eb9aa99226c45e
GJ Roelofs <gj.roelofs@codepoke.net>
2021-02-19 17:46:54 +0100

    Death Announcer mod v1

--- DeathAnnouncerMod/DeathAnnouncerMod.cs
+++ DeathAnnouncerMod/DeathAnnouncerMod.cs
@@ -1,15 +1,23 @@
 using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
-namespace ExampleValheimMod
+namespace DeathAnnouncerValheimMod
 {
     
     [BepInPlugin("com.freecode.mods.valheim.deathannouncermod", "Death Announcer Mod", "1.0.0.0")]
     public class DeathAnnouncerValheimMod : BaseUnityPlugin {
 
         void Awake(){
-            UnityEngine.Debug.Log("Hello, world!");
+            Harmony.CreateAndPatchAll(typeof(DeathAnnouncerValheimMod));
         }
         
+        [HarmonyPatch(typeof(Player), "OnDeath")]
+        [HarmonyPrefix]
+        static void NotifyOtherPlayers(Player __instance){
+            Chat.instance.SendPing(__instance.transform.position);
+            Chat.instance.SendText(Talker.Type.Shout, __instance.GetPlayerName() + " died a horrible death!");
+        }
         
     }
     

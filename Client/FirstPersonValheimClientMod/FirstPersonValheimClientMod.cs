79efab034afd7e25304f49e31dae1a2461e168b8
GJ Roelofs <gj.roelofs@codepoke.net>
2021-02-20 13:44:25 +0100

    Update FPS mod to setup variables in config.

--- Client/FirstPersonValheimClientMod/FirstPersonValheimClientMod.cs
+++ Client/FirstPersonValheimClientMod/FirstPersonValheimClientMod.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 
@@ -9,30 +10,49 @@ namespace Loki.Mods
     
     
     [BepInPlugin("com.loki.clientmods.valheim.firstperson", "First Person Client Mod", "1.0.0.0")]
-    public class FirstPersonValheimClientMod : BaseUnityPlugin
-    {
+    public class FirstPersonValheimClientMod : BaseUnityPlugin {
+
+        // Statics done because injection requires static methods
         private static bool IsFirstPerson = false;
         private static Action<bool> _setVisible;
         private static MethodInfo _setVisibleInfo;
         
-        void Awake(){
+        private static float _oldNearPlane;
+        private static ConfigEntry<float> _fspNearPlane, _eyeOffset;
+        
+        void Awake() {
+            _fspNearPlane = Config.Bind("Camera", "FPSNearPlane", 0.001f, "The Near Plane of the camera during FP mode");
+            _eyeOffset = Config.Bind("Camera", "FPSOffset", -0.1f, "The offset from the eye position the camera is placed");
+            
             _setVisibleInfo = typeof(Character).GetMethod("SetVisible",BindingFlags.NonPublic | BindingFlags.Instance);
             Harmony.CreateAndPatchAll(typeof(FirstPersonValheimClientMod));
         }
         
-        //private Vector3 GameCamera.GetCameraOffset(Player player)
-        //private void GetCameraPosition(float dt, out Vector3 pos, out Quaternion rot)
+        // private void GetCameraPosition(float dt, out Vector3 pos, out Quaternion rot)
         [HarmonyPatch(typeof(GameCamera), "GetCameraPosition")]
         [HarmonyPostfix]
         static void SetCameraPositionToEyeOnFPS(GameCamera __instance, float dt, ref Vector3 pos, ref Quaternion rot) {
 
             // Toggle FPS on H
-            if (Input.GetKeyDown(KeyCode.H))
+            if (Input.GetKeyDown(KeyCode.H)) {
                 IsFirstPerson = !IsFirstPerson;
+                
+                // Setup near plane etc
+                var cam = __instance.GetComponent<Camera>();
 
-            if (IsFirstPerson) {
-                pos = Player.m_localPlayer.m_eye.transform.position;
+                if (IsFirstPerson) {
+                    _oldNearPlane = cam.nearClipPlane;
+                    cam.nearClipPlane = _fspNearPlane.Value;
+                } else {
+                    cam.nearClipPlane = _oldNearPlane;
+                }
             }
+
+            if (!IsFirstPerson) return;
+            
+            var mEye = Player.m_localPlayer.m_eye.transform;
+            pos = mEye.position + mEye.forward * _eyeOffset.Value;
+            
         }
         
         [HarmonyPatch(typeof(Player), "FixedUpdate")]
@@ -40,7 +60,8 @@ namespace Loki.Mods
         static void ForceCharacterModelVisible(Player __instance, ZNetView ___m_nview) {
 
             // See Player code aborting on desync issues
-            if (___m_nview == null || !___m_nview.IsOwner() || Player.m_localPlayer != __instance) return;
+            if (___m_nview == null || ___m_nview.GetZDO() == null || !___m_nview.IsOwner() || Player.m_localPlayer != __instance)
+                return;
             
             // Late bind to instance
             if (_setVisible == null) {

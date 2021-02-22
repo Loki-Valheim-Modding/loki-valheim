df5bea0600f5b2010c0e7286a47ca66b2faf2c1c
Nico van Driel <nico.v.driel@gmail.com>
2021-02-22 19:04:24 +0100

    Changed workbench mod to allow configuration of which pieces and/or which workbenches ignore the requirements check

--- Client/ImmersionModValheimClientMod/BuildWithoutWorkbenchClientMod.cs
+++ Client/ImmersionModValheimClientMod/BuildWithoutWorkbenchClientMod.cs
@@ -13,10 +13,39 @@ namespace ImmersionModValheimClientMod
     public class BuildWithoutWorkbenchClientMod : BaseUnityPlugin
     {
         private ConfigEntry<bool> _configEnabled;
+        private ConfigEntry<string> _configAllowedPieces;
+        private ConfigEntry<string> _configIgnoredStations;
+
+        private static string[] _allowedPieces = new string[]
+        {
+            "$piece_logbeam2",
+            "$piece_logbeam4",
+            "$piece_logpole2",
+            "$piece_logpole4",
+            "$piece_woodbeam1",
+            "$piece_woodbeam2",
+            "$piece_woodbeam26",
+            "$piece_woodbeam45",
+            "$piece_woodpole1",
+            "$piece_woodpole2",
+            "$piece_groundtorchwood",
+            "$piece_sign",
+        };
+
+        private static string[] _ignoredStations = new string[]
+        {
+
+        };
+
 
         void Awake()
         {
             _configEnabled = Config.Bind("Settings", "Enabled", true, "Whether to enable this mod.");
+            _configAllowedPieces = Config.Bind("Settings", "PiecesAllowedWithoutCraftingStation", String.Join(",", _allowedPieces), "A list of individual pieces where the crafting station check is ignored. Use this to allow specific pieces such as poles while still limiting complex pieces like walls and roofs. See https://valheim.fandom.com/wiki/Localization for a list");
+            _configIgnoredStations = Config.Bind("Settings", "IgnoreCraftingStations", String.Join(",", _ignoredStations), "A list of crafting stations to ignore when checking whether you can build something. Use this to instantly allow all wooden building pieces without having a \"$piece_workbench\" nearby, for example");
+
+            _allowedPieces = _configAllowedPieces.Value.Split(',');
+            _ignoredStations = _configIgnoredStations.Value.Split(',');
 
             if (_configEnabled.Value)
             {
@@ -28,13 +57,10 @@ namespace ImmersionModValheimClientMod
         [HarmonyPrefix]
         public static void PlacePiecePre(Player __instance, Piece piece)
         {
-            Console.instance.Print("Place piece pre");
             if (piece.m_craftingStation)
             {
-                Console.instance.Print("Piece requires crafting station");
-                if (piece.m_craftingStation.m_name == "$piece_workbench")
+                if (_ignoredStations.Contains(piece.m_craftingStation.m_name) || _allowedPieces.Contains(piece.m_name))
                 {
-                    Console.instance.Print("Piece requires workbench. Removing.");
                     piece.m_craftingStation = null;
                 }
             }

93567618e25e427ebb63d3df1873773f9b148066
Nico van Driel <nico.v.driel@gmail.com>
2021-02-22 19:09:57 +0100

    Added experimental mode where melee attacks are aimed at where you're looking at, rather than always straight in front of your character

--- Client/FirstPersonValheimClientMod/FirstPersonValheimClientMod.cs
+++ Client/FirstPersonValheimClientMod/FirstPersonValheimClientMod.cs
@@ -25,6 +25,7 @@ namespace Loki.Mods
         private static ConfigEntry<bool> _showBodyWhenAiming;
         private static ConfigEntry<bool> _showBodyWhenBlocking;
         private static ConfigEntry<bool> _jawFix;
+        private static ConfigEntry<bool> _meleeAimFix;
         private static ConfigEntry<bool> _defaultState;
 
         private static Transform _helmetAttach;
@@ -32,9 +33,53 @@ namespace Loki.Mods
         private static Vector3 _originalHeadScale;
         private static Vector3 _originalJawScale;
         private static Transform _head;
+        private static Transform _spine;
         private static bool _isAimingBow;
 
+        private static bool _transitionOutOfAttack;
+
+        private static string[] attacks = new string[]
+        {
+            "swing_sword 1",
+            //"Aim bow",
+            //"bow_fire",
+            //"spear_throw",
+            "bow_fire",
+            //"swing_pickaxe",
+            "Unarmed punch 1",
+            "One handed Blocking",
+            "Unarmed Blocking",
+            "Shield blocking",
+            "knife slash 1",
+            "knife slash 2",
+            "knife slash 3",
+            "swing_sword 0",
+            "swing_sword 2",
+            //"swing_hammer",
+            //"swing_hoe",
+            "Two handed blocking",
+            "swing_sledge",
+            "sword_secondary",
+            "atgeir_attack 0",
+            "atgeir_attack 1",
+            "atgeir_attack 2",
+            "battleaxe_attack 0",
+            "battleaxe_attack 1",
+            "battleaxe_attack 2",
+            "swing_axe 0",
+            "swing_axe 1",
+            "swing_axe 2",
+            "Unarmed punch 2",
+            "Unarmed kick",
+            "knife secondary",
+            "atgeir_secondary",
+            "spear_poke",
+            "battleaxe_powerattack",
+            "mace_secondary",
+        };
+
         private static FirstPersonValheimClientMod _thisMod;
+        private static int _currentTransitionAnim;
 
         void Awake()
         {
@@ -43,7 +88,8 @@ namespace Loki.Mods
             _hotkey = Config.Bind("Controls", "Hotkey", new KeyboardShortcut(KeyCode.H), "Hotkey used to cycle between first person modes");
             _showBodyWhenAiming = Config.Bind("Body", "ShowBodyWhenAiming", false, "Whether to show your body while aiming your bow. The bow obscures the center of your screen so you might want to disable it");
             _showBodyWhenBlocking = Config.Bind("Body", "ShowBodyWhenBlocking", false, "Whether to show your body while blocking. Some shields might obscure your vision, but that's what shields are for!");
-            _jawFix = Config.Bind("Body", "JawFix", false, "Tries to fix the visible jaw when helmet is set to be shown (even when not wearing a helmet). Might cause other artifacts for certain helmets.");
+            _jawFix = Config.Bind("Body", "JawFix", false, "[Experimental] Tries to fix the visible jaw when helmet is set to be shown (even when not wearing a helmet). Might cause other artifacts for certain helmets.");
+            _meleeAimFix = Config.Bind("Body", "MeleeAimFix", false, "[Experimental] Changes the default melee attack direction (which is always straight forward from your body) into a direction based on your head camera.");
             _defaultState = Config.Bind("Controls", "StartsEnabled", true);
 
             _setVisibleInfo = typeof(Character).GetMethod("SetVisible", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -58,6 +104,77 @@ namespace Loki.Mods
             {
                 _thisMod.StartCoroutine(SmallSpawndelay());
             }
+        }
+
+
+        [HarmonyPatch(typeof(Attack), "Clone")]
+        [HarmonyPostfix]
+        static void AttackAwakePost(Attack __instance, ref Attack __result)
+        {
+            Console.instance.Print("New instance of attack has been cloned");
+
+            if (_currentFPMode == FirstPersonModes.ThirdPerson)
+                return;
+
+            __result.m_attackHeight = 0;
+        }
+
+        [HarmonyPatch(typeof(Attack), "GetMeleeAttackDir")]
+        [HarmonyPostfix]
+        static void OverrideGetMeleeAttackDir(Attack __instance, ref Transform originJoint, ref Vector3 attackDir)
+        {
+            if (_currentFPMode == FirstPersonModes.ThirdPerson || !_meleeAimFix.Value)
+                return;
+
+            var m_character = (Humanoid)AccessTools.Field(typeof(Attack), "m_character").GetValue(__instance);
+            if (Player.m_localPlayer != m_character)
+                return;
+
+            //originJoint = _spine.transform;
+            originJoint = GameCamera.instance.transform;
+            //originJoint = _head.transform;
+            attackDir = originJoint.forward;
+            //attackDir = Vector3.RotateTowards(m_character.transform.forward, aimDir, 0.017453292f * __instance.m_maxYAngle, 10f);
+        }
+
+        [HarmonyPatch(typeof(Player), "LateUpdate")]
+        [HarmonyPostfix]
+        static void LateUpdate(Player __instance)
+        {
+            if (_currentFPMode == FirstPersonModes.ThirdPerson || !_meleeAimFix.Value ||  Player.m_localPlayer != __instance)
+                return;
+
+            var animator = __instance.GetComponentInChildren<Animator>();
+            var currentState = animator.GetCurrentAnimatorStateInfo(0);
+            var nextState = animator.GetNextAnimatorStateInfo(0);
+
+            foreach (var attack in attacks)
+            {
+                if (currentState.IsName(attack))
+                {
+                    //Console.instance.Print("In attack state");
+                    _spine.rotation = GameCamera.instance.transform.rotation;
+                    _transitionOutOfAttack = true;
+                    _currentTransitionAnim = nextState.shortNameHash;
+
+                }
+                else if (nextState.IsName(attack))
+                {
+                    _transitionOutOfAttack = false;
+                    _spine.rotation = Quaternion.RotateTowards(_spine.rotation, GameCamera.instance.transform.rotation, 360 * nextState.normalizedTime);
+                    //Console.instance.Print("Transitioning to attack state, time " + nextState.normalizedTime);
+                }
+                //TODO bugged
+                //else if (_transitionOutOfAttack)
+                //{
+                //    if (_currentTransitionAnim == currentState.shortNameHash)
+                //    {
+                //        _spine.rotation = Quaternion.RotateTowards(GameCamera.instance.transform.rotation, _spine.rotation, 360 * nextState.normalizedTime);
+                //    }
+                //}
+            }
+
+            //_helmetAttach.rotation = GameCamera.instance.transform.rotation;
         }
 
         [HarmonyPatch(typeof(Player), "OnDeath")]
@@ -140,6 +257,7 @@ namespace Loki.Mods
                 __instance.m_nearClipPlaneMin = _fspNearPlane.Value;
                 __instance.m_nearClipPlaneMax = _fspNearPlane.Value;
                 _head = FindTransform(Player.m_localPlayer.transform, "Visual", "Armature", "Hips", "Spine", "Spine1", "Spine2", "Neck", "Head");
+                _spine = FindTransform(Player.m_localPlayer.transform, "Visual", "Armature", "Hips", "Spine", "Spine1");
                 _helmetAttach = FindTransform(_head, "Helmet_attach");
                 _jaw = FindTransform(_head, "Jaw");
                 _originalHeadScale = _head.localScale;

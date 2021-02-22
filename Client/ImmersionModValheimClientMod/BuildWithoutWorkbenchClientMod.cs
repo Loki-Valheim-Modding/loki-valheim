36c00d4591cea6d3e4250c534727c71e3aaee914
GJ Roelofs <gj.roelofs@codepoke.net>
2021-02-22 20:10:43 +0100

    Trim input on workbench.

--- Client/ImmersionModValheimClientMod/BuildWithoutWorkbenchClientMod.cs
+++ Client/ImmersionModValheimClientMod/BuildWithoutWorkbenchClientMod.cs
@@ -44,8 +44,8 @@ namespace ImmersionModValheimClientMod
             _configAllowedPieces = Config.Bind("Settings", "PiecesAllowedWithoutCraftingStation", String.Join(",", _allowedPieces), "A list of individual pieces where the crafting station check is ignored. Use this to allow specific pieces such as poles while still limiting complex pieces like walls and roofs. See https://valheim.fandom.com/wiki/Localization for a list");
             _configIgnoredStations = Config.Bind("Settings", "IgnoreCraftingStations", String.Join(",", _ignoredStations), "A list of crafting stations to ignore when checking whether you can build something. Use this to instantly allow all wooden building pieces without having a \"$piece_workbench\" nearby, for example");
 
-            _allowedPieces = _configAllowedPieces.Value.Split(',');
-            _ignoredStations = _configIgnoredStations.Value.Split(',');
+            _allowedPieces = _configAllowedPieces.Value.Split(',').Select(n => n.Trim()).ToArray();
+            _ignoredStations = _configIgnoredStations.Value.Split(',').Select(n => n.Trim()).ToArray();
 
             if (_configEnabled.Value)
             {

4054d8da1975e715780f85a7f74aeb4f4140d315
Devedse <davydavidse@gmail.com>
2021-02-24 21:00:25 +0100

    Added beppy

--- Scripts/SetupBuildEnv.ps1
+++ Scripts/SetupBuildEnv.ps1
@@ -9,7 +9,7 @@ if (!(Test-Path "Build/SteamCMD" -PathType Container)) {
 if (!(Test-Path "Build/SteamCMD/steamcmd.exe" -PathType Leaf)) {
     Write-Host "Downloading SteamCMD"
     Invoke-WebRequest -Uri "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" -OutFile "Build/SteamCMD/steamcmd.zip"
-    Expand-Archive -LiteralPath 'Build/SteamCMD/steamcmd.zip' -DestinationPath 'Build/SteamCMD/' -Force
+    Expand-Archive -LiteralPath "Build/SteamCMD/steamcmd.zip" -DestinationPath "Build/SteamCMD/" -Force
 }
 
 #Game
@@ -29,6 +29,10 @@ Copy-Item "Build/ValheimServer/valheim_server_Data/Managed/*.dll" "Build/Libs"
 
 Invoke-WebRequest -Uri "https://cdn.discordapp.com/attachments/623910091132895232/809851661975420989/unstripped_managed.7z" -OutFile "Build/unstripped_managed.7z"
 & 7z e -y "Build/unstripped_managed.7z" -oBuild/Libs
+
+Invoke-WebRequest -Uri "https://github.com/BepInEx/BepInEx/releases/download/v5.4.5/BepInEx_x64_5.4.5.0.zip" -OutFile "Build/BepInEx_x64.zip"
+Expand-Archive -LiteralPath "Build/BepInEx_x64.zip" -DestinationPath "Build/Libs/" -Force
+
 
 if (!(Test-Path "Libs" -PathType Container)) {
     New-Item "Libs" -ItemType "directory"

751fae39e5419d45a0f75043f74d5f6a888abd4a
Devedse <davydavidse@gmail.com>
2021-02-28 17:28:42 +0100

    Some logging

--- Scripts/VersionBepInPlugins.ps1
+++ Scripts/VersionBepInPlugins.ps1
@@ -13,6 +13,8 @@ $csharpFiles = Get-ChildItem -Path $startDir -Recurse -Include *.cs
 
 $regexString = '\[BepInPlugin\(\"com\.loki\.clientmods\.valheim\..+\"\, \".+\"\, \"(\d+\.\d+\.\d+\.\d+)\"\)\]'
 
+$replaceCount = 0
+
 foreach ($csharpFile in $csharpFiles) {
     $content = Get-Content $csharpFile | Out-String
 
@@ -21,12 +23,19 @@ foreach ($csharpFile in $csharpFiles) {
         $m2 = $Matches[1]
 
         $result = $m1.Replace($m2, $desiredVersion)
-        $newContent = $content.Replace($m1, $result)
+        if ($m1 -ne $result) {
+            $newContent = $content.Replace($m1, $result)
 
-        Set-Content -Path $csharpFile -Value $newContent
+            Set-Content -Path $csharpFile -Value $newContent
 
-        Write-Host "Replacing content of file: $csharpFile"
-        Write-Host $newContent
-        Write-Host ""
+            Write-Host "Replacing content of file: $csharpFile"
+            Write-Host $newContent
+            Write-Host ""
+            $replaceCount++
+        } else {
+            Write-Host "Skipping $csharpFile because the expected version number was already in this file."
+        }
     }
 }
+
+Write-Host "Version script completed. Version numbers replaced: $replaceCount"
